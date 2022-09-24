00bf0f143a93d5af5337d0e94e19f65611564753
WSXCXFAS <114204493+WSXCXFAS@users.noreply.github.com>
2022-09-24 14:08:35 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -51,7 +51,7 @@ export const USER_CONFIG = {
   
   // 每日一言的内容类型
   // 可以填写【动画，漫画，游戏，小说，原创，网络，其他】； 随机则填写 ""
-  LITERARY_PREFERENCE: "",
+  LITERARY_PREFERENCE: "小说",
   
   
   /**
@@ -81,7 +81,7 @@ export const USER_CONFIG = {
         {"type": "*生日", "name": "冰冰", "year": "2022", "date": "08-10"},
         {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {"type": "生日", "name": "冰冰", "year": "2002", "date": "08-31"},
+        {"type": "生日", "name": "冰冰", "year": "2002", "date": "08-10"},
         {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
       ],
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
